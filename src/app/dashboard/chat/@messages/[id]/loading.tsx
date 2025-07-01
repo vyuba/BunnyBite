7fc71cd7c199018a18480e0ea5434e4c7bda4605
vyuba <preyealexander731@gmail.com>
2025-07-01 02:18:51 +0100

    customized the loading spinner for the parralel route

--- src/app/dashboard/chat/@messages/[id]/loading.tsx
+++ src/app/dashboard/chat/@messages/[id]/loading.tsx
@@ -1,7 +1,22 @@
+"use client";
 import LoadingSpinner from "@/components/LoadingSpinner";
+import { useChatProvider } from "@/app/providers/SidebarStoreProvider";
 
 const Loading = () => {
-  return <LoadingSpinner />;
+  const { isChatOpen } = useChatProvider();
+  return (
+    <div
+      className={`w-full ${
+        isChatOpen ? "translate-x-full z-10" : " translate-x-0 z-20  "
+      } md:translate-x-0 md:z-0  h-full relative duration-300  bg-white overflow-hidden rounded-md border border-[#E3E3E3]`}
+    >
+      <div className="w-full h-full flex items-center bg-white justify-center overflow-hidden rounded-md border border-[#E3E3E3]">
+        <div className="flex flex-col items-center">
+          <LoadingSpinner />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
